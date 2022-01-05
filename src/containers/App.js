4cc0717544e81141dcb8feaa00ddf12b8d9d918f
DandyDo <alqasimtoon@gmail.com>
2022-01-05 21:23:14 +0400

    replaced app class with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,58 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll.js';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css'; 
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state =  {
-            robots: [],
-            searchfield: ''
-        }
-    }
-    
-    onSearchChange = (event) => {
-        this.setState({ searchfield : event.target.value });
-    }
-
-    render() {
-        const { robots, searchfield } = this.state;
-        // Filter the robots based on their name (always lowercase)
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-
-        // Display 'Loading' if there are too many users, else display the robots
-        if (!robots.length) {
-            return <h1 className="tc">Loading</h1>;
-        } 
-        else {
-            return (
-                <div className="tc">
-                    <h1 className="f1">
-                        <a href="https://github.com/DandyDo/robofriends" className=" dim" style={{color:'inherit', textDecoration: 'inherit'}}>
-                            Robofriends
-                        </a>
-                    </h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>   
-            );
-        }
-    }
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchField] = useState('');
 
     // Fake online REST API that contains users
-    componentDidMount () {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => {this.setState({ robots: users })});
+            .then(users => {setRobots(users)});
+    }, []);
+    
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
+    }
+
+    // Filter the robots based on their name (always lowercase)
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    // Display 'Loading' if there are too many users, else display the robots
+    if (!robots.length) {
+        return <h1 className="tc">Loading</h1>;
+    } 
+    else {
+        return (
+            <div className="tc">
+                <h1 className="f1">
+                    <a href="https://github.com/DandyDo/robofriends" className=" dim" style={{color:'inherit', textDecoration: 'inherit'}}>
+                        Robofriends
+                    </a>
+                </h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+            </div> 
+        )  
     }
 }
 
